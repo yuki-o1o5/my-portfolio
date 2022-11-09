c58cc9c6675269663a03427d1f1d292dd1f76a1c
yuki-o1o5 <jkksst1712@gmail.com>
2022-11-08 16:45:24 -0800

    still not work

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -27,43 +27,39 @@ export async function getStaticProps({ paths }) {
   }
 }
 
-// export async function getStaticPaths() {
-//   const res = await axios.get(`https://dev.to/api/articles/me/published`, {
-//     headers: {
-//       "api-key": process.env.DEV_TO_API_KEY,
-//     },
-//   });
+export async function getStaticPaths() {
+  const res = await axios.get(`https://dev.to/api/articles/me/published`, {
+    headers: {
+      "api-key": process.env.DEV_TO_API_KEY,
+    },
+  });
 
-//   const posts = res.data;
-//   console.log(posts);
+  const posts = res.data;
+  console.log(posts);
+
+  const paths = posts.map((post) => ({
+    params: {
+      id: post.id.toString(),
+    },
+  }));
+
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+// export async function getStaticPaths() {
+//   const url = `https://dev.to/api/articles/me/published`;
+//   const response = await fetch(url,header);
+//   const posts = await response;
+//   console.log("hello ", posts);
 
 //   const paths = posts.map((post) => ({
 //     params: {
 //       id: post.id.toString(),
 //     },
 //   }));
-
-//   return {
-//     paths: [],
-//     fallback: "blocking",
-//   };
-// }
-
-// export async function getStaticPaths() {
-//   const url = `https://dev.to/api/articles/me/published`;
-//   const response = await fetch(url, );
-
-//   console.log("responese", response);
-//   const posts = await response;
-//   console.log("hello ", posts);
-
-//   const paths = "";
-
-//   // const paths = posts.map((post) => ({
-//   //   params: {
-//   //     id: post.id.toString(),
-//   //   },
-//   // }));
 
 //   return {
 //     paths: [],
