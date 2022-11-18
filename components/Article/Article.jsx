c3ca07b4a43e732dc05c95d536069e23be9296a4
yuki-o1o5 <jkksst1712@gmail.com>
2022-11-17 19:33:31 -0800

    modify2

--- components/Article/Article.jsx
+++ components/Article/Article.jsx
@@ -5,7 +5,7 @@ import styles from "./Article.module.css";
 const Article = (props) => {
   return (
     <div className={styles.article}>
-      <a href={props.url} target="_blank">
+      <a href={props.url}>
         <h3>{props.articletitle}</h3>
         <p className={styles.articledate}>{props.date}</p>
       </a>
