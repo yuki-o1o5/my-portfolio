4dca4990104eab682f954e9be889ec31417abba6
yuki-o1o5 <jkksst1712@gmail.com>
2022-11-17 19:42:31 -0800

    add rel

--- components/Herosection/Herosection.jsx
+++ components/Herosection/Herosection.jsx
@@ -23,19 +23,20 @@ const Herosection = () => {
         est pellentesque elit ullamcorper dignissim cras tincidunt. In hac
       </p>
       <div className={styles.icon}>
-        <a href="https://github.com/yuki-o1o5" target="_blank">
+        <a href="https://github.com/yuki-o1o5" target="_blank" rel="noreferrer">
           <i class="fa-brands fa-square-github fa-2x fa-fw"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/yuki-kasugai-136826247/"
           target="_blank"
+          rel="noreferrer"
         >
           <i class="fa-brands fa-linkedin fa-2x fa-fw"></i>
         </a>
         <a href="mailto:sample@sample.com">
           <i class="fa-solid fa-square-envelope fa-2x fa-fw"></i>
         </a>
-        <a href="#" target="_blank">
+        <a href="#" target="_blank" rel="noreferrer">
           <i class="fa-solid fa-square-poll-horizontal  fa-2x fa-fw"></i>
         </a>
       </div>
