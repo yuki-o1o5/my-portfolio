2d763f50fa0ada2c241206a4483fd1b86e0de3d5
yuki-o1o5 <jkksst1712@gmail.com>
2023-07-11 18:30:14 -0700

    docs: modify date

--- components/Article/Article.jsx
+++ components/Article/Article.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import styles from "./Article.module.css";
 
 const Article = (props) => {
+  const modifiedDate = props.date.slice(0, 10);
   return (
     <div className={styles.article}>
       <a target="_blank" rel="noreferrer" href={props.url}>
         <h3>{props.articletitle}</h3>
-        <p className={styles.articledate}>{props.date}</p>
+        <p className={styles.articledate}>{modifiedDate}</p>
       </a>
     </div>
   );
