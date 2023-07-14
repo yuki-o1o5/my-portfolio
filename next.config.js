5a5b358aa1b16f949785f81851ef21885d9e89b1
yuki-o1o5 <jkksst1712@gmail.com>
2023-07-13 19:28:45 -0700

    chore: add config to show img

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "raw.githubusercontent.com",
+      "www.vectorlogo.zone",
+      "www.mysql.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
